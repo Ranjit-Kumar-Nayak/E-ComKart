e63232b6e55c406e0d6e5b768e3420fe1edceff6
ranjit <ranjitkumarnayak420@gmail.com>
2025-02-10 11:07:39 +0530

    basic setups

--- EComKart/src/main/java/com/ranjit/EComKart/controller/HomeController.java
+++ EComKart/src/main/java/com/ranjit/EComKart/controller/HomeController.java
@@ -1,13 +1,16 @@
 package com.ranjit.EComKart.controller;
 
+import com.ranjit.EComKart.response.ApiResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
     @GetMapping
-    public String Display(){
-        return "Hello World";
+    public ApiResponse Display(){
+       ApiResponse apiResponse= new ApiResponse();
+       apiResponse.setMessage("hello world !!!!!!!!");
+       return apiResponse;
 
     }
 }
