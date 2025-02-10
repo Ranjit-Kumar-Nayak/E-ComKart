69e7f07f70eff1e33d12ba1273c79071a09106f3
ranjit <ranjitkumarnayak420@gmail.com>
2025-02-10 08:22:13 +0530

    initial setup of db

---
+++ EComKart/src/main/java/com/ranjit/EComKart/EComKartApplication.java
@@ -0,0 +1,13 @@
+package com.ranjit.EComKart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EComKartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EComKartApplication.class, args);
+	}
+
+}
---
+++ EComKart/src/test/java/com/ranjit/EComKart/EComKartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranjit.EComKart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComKartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
